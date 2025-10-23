8134bbe2b1e4936c115ebcb7c3038b02553a25f0
Prabhakar PAl <prabhakar.ms.pal@gmail.com>
2025-10-23 21:53:54 +0530

    added flyway migration

--- OrderService/src/main/java/com/prabhakar/OrderService/Order.java
+++ OrderService/src/main/java/com/prabhakar/OrderService/Order.java
@@ -1,10 +1,10 @@
 package com.prabhakar.OrderService;
 
 import java.math.BigDecimal;
-import org.springframework.data.annotation.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
