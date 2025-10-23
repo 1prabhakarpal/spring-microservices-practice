4d20fd66b0aeb56a81d321c05fc938d0cb91f6e5
Prabhakar PAl <prabhakar.ms.pal@gmail.com>
2025-10-23 22:05:27 +0530

    written intertaion test to place order

--- OrderService/src/test/java/com/prabhakar/OrderService/OrderServiceApplicationTests.java
+++ OrderService/src/test/java/com/prabhakar/OrderService/OrderServiceApplicationTests.java
@@ -1,15 +1,54 @@
 package com.prabhakar.OrderService;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import io.restassured.RestAssured;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.3.0");
 
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mysqlContainer.start();
+    }
+
+    @Test
+    void shouldPlaceOrder() {
+        String orderRequest = """
+                {
+                    "skuCode": "iphone_16",
+                    "quantity": 2,
+                    "price": 1000
+                }
+                """;
+
+        given().contentType("application/json")
+                .body(orderRequest)
+                .when()
+                .post("/api/order")
+                .then()
+                .statusCode(200)
+                .body(containsString("Order placed successfully with Order Number:"));
+    }
 }
